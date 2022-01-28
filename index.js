8b65e9da230c4366b214052a0f5449201d8a55d1
LogicStudios75 <logicstudios75@gmail.com>
2022-01-28 20:32:21 +0530

    initialize the repo

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+require("dotenv").config();
+
+const client = new Discord.Client({
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content == "hi") {
+    message.reply("hey!");
+  }
+});
+
+client.login(process.env.TOKEN);
